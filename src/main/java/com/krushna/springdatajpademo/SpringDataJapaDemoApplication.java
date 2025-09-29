8aba767420e33a1d7db1316b78dba063cb1fb9b6
Krushna Musmade <krushnamusmade90@gmail.com>
2025-09-29 12:49:02 +0530

    Initial Spring Boot project upload

---
+++ src/main/java/com/krushna/springdatajpademo/SpringDataJapaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.krushna.springdatajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJapaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJapaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krushna/springdatajpademo/SpringDataJapaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krushna.springdatajpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJapaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
